9020f3b68d7cd7fa06acb5fd7e4f91232f3a4f63
Tatsuro Hisamori <medianetworks@gmail.com>
2013-01-18 19:02:39 +0900

    initial commit

---
+++ lib/Riak/Lite.pm
@@ -0,0 +1,32 @@
+package Riak::Lite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Riak::Lite -
+
+=head1 SYNOPSIS
+
+  use Riak::Lite;
+
+=head1 DESCRIPTION
+
+Riak::Lite is
+
+=head1 AUTHOR
+
+Tatsuro Hisamori E<lt>myfinder@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
